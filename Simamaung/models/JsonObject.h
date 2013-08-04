752023260f5775f1cf7e9d0489b63619e562aeb0
Arie <arie@wego.com>
2013-08-04 12:37:51 +0700

    add JsonObjectModel Set it as Global Models

---
+++ Simamaung/models/JsonObject.h
@@ -0,0 +1,13 @@
+//
+//  JsonObject.h
+//  Simamaung
+//
+//  Created by Arie on 8/4/13.
+//  Copyright (c) 2013 netra. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JsonObject : NSObject
+
+@end
---
+++ Simamaung/models/JsonObject.m
@@ -0,0 +1,13 @@
+//
+//  JsonObject.m
+//  Simamaung
+//
+//  Created by Arie on 8/4/13.
+//  Copyright (c) 2013 netra. All rights reserved.
+//
+
+#import "JsonObject.h"
+
+@implementation JsonObject
+
+@end
